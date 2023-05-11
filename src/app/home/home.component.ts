cdd17788e734e6ec970b463eb047c4eb3054ac23
wilnermena <wi_an9@hotmail.com>
2023-05-11 12:03:59 -0400

    segundo2

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,19 +7,40 @@ import { ApiService } from '../service/api.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  data: any[]=[];
-
+  data: any[] = [];
+  data1:any[]=[];
 
   constructor(private apiService:ApiService) {}
 
   ngOnInit(): void {
     this.llenarData()
+    this.llenarData1()
   }
 
   llenarData(){
-    this.apiService.getData().subscribe(data=>{
-      this.data=data;
+    this.apiService.getData().subscribe(response=>{
+      if(Array.isArray(response)){
+        this.data=response;
+      }
+      else{
+        this.data=[response];
+      }
+      
       console.log(this.data);
+    })
+
+  }
+
+  llenarData1(){
+    this.apiService.getData1().subscribe(response=>{
+      if(Array.isArray(response)){
+        this.data1=response;
+      }
+      else{
+        this.data1=[response];
+      }
+      
+      console.log(this.data1);
     })
 
   }
--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -6,12 +6,17 @@ import {Observable} from 'rxjs';
   providedIn: 'root'
 })
 export class ApiService {
- private urlAPi='https://rickandmortyapi.com/api/character/1,2,3,4';
-
+ private urlrick='https://rickandmortyapi.com/api/character/1,2,3,4';
+ private urlthrone="https://thronesapi.com/api/v2/Characters/2";
   constructor( private http: HttpClient) { }
 
   public getData(): Observable<any>{
-    return this.http.get<any>(this.urlAPi);
+    return this.http.get<any>(this.urlrick);
+ 
+  }
+  public getData1(): Observable<any>{
+    return this.http.get<any>(this.urlthrone);
+ 
   }
 
 }
